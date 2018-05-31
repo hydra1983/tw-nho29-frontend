6f69fb34b8aaa22069d41b6da2855a4a2ec305c9
Edison Guo <hydra1983@gmail.com>
2018-05-31 10:49:59 +0800

    [fguo] 修复 lint

--- src/routes/My/TaskCard.js
+++ src/routes/My/TaskCard.js
@@ -1,7 +1,3 @@
 import React from 'react';
-import { Link } from 'dva/router';
-import Exception from 'components/Exception';
 
-export default () => (
-  <div>任务卡</div>
-);
+export default () => <div>任务卡</div>;
